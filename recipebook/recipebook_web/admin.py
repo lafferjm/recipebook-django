e7ff2d7fff7ba75e11f8502546aa4144fec82ef4
Justin Lafferty <lafferjm@gmail.com>
2020-01-13 08:42:30 -0600

    recipes model and admin registration

--- recipebook/recipebook_web/admin.py
+++ recipebook/recipebook_web/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'subcategory')
 
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'category')
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- recipebook/recipebook_web/models.py
+++ recipebook/recipebook_web/models.py
@@ -8,7 +8,19 @@ class Category(models.Model):
 
     name = models.CharField(max_length=30)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    subcategory = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    subcategory = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='subcategories')
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=30)
+    recipe = models.TextField()
+    ingredients = models.TextField()
+    found = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
