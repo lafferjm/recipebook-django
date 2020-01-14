44bb14e3207afd0ae6ec2e462679dd0d17566841
Justin Lafferty <lafferjm@gmail.com>
2020-01-14 07:30:30 -0600

    user can login

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view())
 ]
