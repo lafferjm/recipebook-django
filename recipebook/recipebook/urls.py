d2bb35053bc3125503c86fc7fce5168b70c6f355
Justin Lafferty <lafferjm@gmail.com>
2020-01-14 07:36:53 -0600

    debug template to show logged in user

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('recipebook/', include('recipebook_web.urls')),
+    path('', RedirectView.as_view(url='/recipebook/')),
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view())
 ]
