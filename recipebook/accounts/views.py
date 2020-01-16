34fb395314c25f82581e0b61f1c7ece412e72419
Justin Lafferty <lafferjm@gmail.com>
2020-01-15 21:32:31 -0600

    fix user creation

--- recipebook/accounts/views.py
+++ recipebook/accounts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 
 
-class SignUp(FormView):
+class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
