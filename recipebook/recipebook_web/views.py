4e54c9c30fca7f7b6f255cc8caa50996fc4be473
Justin Lafferty <lafferjm@gmail.com>
2020-01-16 20:45:36 -0600

    url and dummy view

--- recipebook/recipebook_web/views.py
+++ recipebook/recipebook_web/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import Category
+from .models import Recipe
 
-class CategoryListView(LoginRequiredMixin, ListView):
-    model = Category
+
+class RecipeListView(LoginRequiredMixin, ListView):
+    model = Recipe
+    template = 'recipebook_web/recipe_list.html'
 
     def get_queryset(self):
-        return Category.objects.filter(pk=self.request.user.id)
+        return Recipe.objects.filter(owner=self.request.user, category__name=self.kwargs['category'])
+
 
 def index(request):
     return render(request, 'recipebook_web/index.html')
