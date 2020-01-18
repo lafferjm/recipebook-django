686d0416713957c6c68d9bb6e7479a2628a6d93f
Justin Lafferty <lafferjm@gmail.com>
2020-01-18 10:34:24 -0600

    configure deployment

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,5 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 LOGIN_REDIRECT_URL = '/'
+
+if os.getenv('DATABASE_URL'):
+    import dj_database_url
+    DEBUG = False
+    ALLOWED_HOSTS.append('.herokuapp.com')
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
